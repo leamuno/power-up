b1011a9a9fc0a17577bca923981c2d4647f552b3
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-02-20 17:14:02 +0900

    Updates user controller so that first and last name are saved as well as a photo and can add new power

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -7,7 +7,26 @@ class PowersController < ApplicationController
   def show
     @power = Power.find(params[:id])
     @booking = Booking.new
-    @bookings = @power.bookings.where(user: current_user) 
+    @bookings = @power.bookings.where(user: current_user)
   end
 
+  def new
+    @power = Power.new()
+  end
+
+  def create
+    @power = Power.new(power_params)
+    @power.user = current_user
+    if @power.save
+      redirect_to powers_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description, :category, :photo, :price)
+  end
 end
