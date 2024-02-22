40796d02c80ac978f8cfb16b081ddb2c1e181c93
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-02-22 15:21:34 +0900

    Adds user profile page and functionality as well as the ability for a user to add and remove powers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :powers, only: ["index", "show", "new", "create"] do
+  resources :powers do
     resources :bookings, only: ["create"]
   end
 
