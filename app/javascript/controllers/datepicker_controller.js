e7072af3b900140e58cbf372e81843e51c2c4239
Noah Endo <noah.eyeing@gmail.com>
2024-02-22 20:41:24 +0900

    made flatpickr great again

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -2,14 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
+
 export default class extends Controller {
-  static values = { default: String }
+  static values = { default: String, unavailable: Array }
 
   connect() {
-    flatpickr(this.element, {
-      defaultDate: this.defaultValue,
-      dateFormat: "l, F j",
+    const end_date = document.querySelector('#booking_end_date').value
+    const end_date_input = document.querySelector('#booking_end_date')
+
+    const config = {
+      "plugins": [new rangePlugin({ input: "#booking_end_date"})],
+      disable: this.unavailableValue,
       mode: 'range',
+      dateFormat: "Y-m-d",
+      defaultDate: this.defaultValue,
+    };
+
+    flatpickr(this.element, config, {
     });
+
+    end_date_input.value = end_date;
   }
 }
