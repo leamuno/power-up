bb672cc79667dc7ca8bb719fd87760c963183a4d
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-02-23 14:39:47 +0900

    Adds more seeds and fixes the search bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @powers = Power.all.sample(3)
-    @recent_powers = Power.order(created_at: :desc).limit(3)
+    @powers = Power.all.where.not(user: current_user).sample(3)
+    @recent_powers = Power.where.not(user: current_user).order(created_at: :desc).limit(3)
   end
 end
